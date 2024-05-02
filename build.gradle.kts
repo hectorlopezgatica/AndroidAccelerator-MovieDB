06b86b992a64948160e76f8943a62740bc461f78
hectorlopezgatica <116033767+hectorlopezgatica@users.noreply.github.com>
2024-05-02 15:28:01 -0400

    Update ksp to 1.0.20 and agp 8.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
 }
